7f7bf1579ad31aec4fd64c07c15782b88242cecc
0eale0 <RUSLIKPVP@yandex.ru>
2020-11-30 21:54:51 +0300

    add place_ship in skeleton, just for test

--- battle_skeleton.py
+++ battle_skeleton.py
@@ -9,31 +9,28 @@
 '''
 # пусть пока будет заданое поле, потом надо будет сделать чтоб игрок сам задовал
 # не придирайся если где-то корабли стоят не по правилам
-battlefield = [
-    [0, 2, 2, 0, 1, 2, 3, 2, 2, 0],
-    [0, 0, 1, 0, 1, 0, 1, 1, 1, 0],
-    [0, 2, 3, 0, 1, 1, 1, 3, 1, 0],
-    [0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 'z', 'z', 'z', 0, 0, 0, 0],
-    [0, 0, 0, 'z', 2, 'z', 0, 0, 0, 0],
-    [0, 0, 0, 'z', 2, 'z', 0, 0, 0, 0],
-    [0, 0, 0, 'z', 'z', 'z', 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
-move = 0
-# потом сделаю проверку на отсутствие кораблей у противника и добавлю в while
-while move != 100:
-    coordinates = [int(i) for i in input('x/y: ').split()]
-    x = coordinates[0]
-    y = coordinates[1]
-    if 0 <= x <= 9 and 0 <= y <= 9:
-        if battlefield[x][y] % 2 == 0:
-            battlefield[x][y] += 1
-            for i in battlefield:
-                print(*i)
-            move += 1
-        else:
-            print('Невозможный ход')
-    else:
-        print('Неверно введены координаты')
+field = [[0] * 10 for i in range(10)]
+x = 3
+y = 3
+size = 6
+orientation = 'vertical'
+
+
+for index in range(size):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            field[y + i][x + j] = 'z'
+
+    field[y][x] = size
+    y += 1
+
+y = 3
+
+for index in range(size):
+    field[y][x] = size
+    y += 1
+
+
+
+for line in field:
+    print(*line)

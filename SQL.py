168c73cf04118b4881410d0d0beb2b8abdc420cd
Belligraf <belligraf@mail.ru>
2020-12-05 23:29:50 +0300

    add comments

--- SQL.py
+++ SQL.py
@@ -1,15 +1,16 @@
 import sqlite3
-import json  # надо добавить импорт доски и счетчик номер игры
+import json  # надо добавить импорт поля и счетчик номер игры
 
 
 class SaveGame:
-    def __init__(self, number_game):
+    def __init__(self, number_game: int):
         self.con = sqlite3.connect("data_save_base_№" + number_game + ".db")  # создает файл где хранятся данные
         self.cursor = self.con.cursor()  # создает курсор для управления таблицой
         self.cursor.execute("create table save_field (player1 enum, player2 enum, one_or_zero TINYINT)")
-        # создает столбцы для таблички где player1, player2 и one_or_zero название стобцов
+        # создает таблицу save_field и столбцы для таблички где player1, player2 и one_or_zero название стобцов
+        # если one_or_zero = 1, то ходит первый игрок иначе второй
 
-    def __add__(self, field_player1, field_player2, one_or_zero):
+    def __add__(self, field_player1: str, field_player2: str, one_or_zero: int):
         self.cursor.execute("INSERT into save_field values (?, ?, ?)", (field_player1, field_player2, one_or_zero))
         # добавляет значение в столбцы
 
@@ -17,7 +18,7 @@ class SaveGame:
         self.cursor.execute("DELETE FROM save_field")  # удаляет всю инфу с таблицы
 
     def save(self):
-        self.con.commit()  # сохраняет таблицу
+        self.con.commit()  # сохраняет таблицу обязательно делай иначе ничего не сохранится
 
     def unpacking(self):
         for row in self.cursor.execute(

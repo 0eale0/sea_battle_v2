5249ec9b42e3dc22649cd4245f5d388f44880128
Belligraf <belligraf@mail.ru>
2020-12-06 11:36:28 +0300

    levelup SQL file

--- SQL.py
+++ SQL.py
@@ -3,26 +3,31 @@ import json  # надо добавить импорт поля и счетчик
 
 
 class SaveGame:
-    def __init__(self, number_game: int):
-        self.con = sqlite3.connect("data_save_base_№" + number_game + ".db")  # создает файл где хранятся данные
+    def __init__(self, number_game: str):
+        self.con = sqlite3.connect("data_base.db")  # открывает файл где хранятся данные
         self.cursor = self.con.cursor()  # создает курсор для управления таблицой
-        self.cursor.execute("create table save_field (player1 enum, player2 enum, one_or_zero TINYINT)")
-        # создает таблицу save_field и столбцы для таблички где player1, player2 и one_or_zero название стобцов
+        self.cursor.execute(
+            "create table save_field_" + number_game + "(player1 enum, player2 enum, one_or_zero TINYINT)")
+
+        # создает таблицу save_field_{number_game} и столбцы для таблички
+        # где player1, player2 и one_or_zero название стобцов
         # если one_or_zero = 1, то ходит первый игрок иначе второй
+        self.con.commit()  # сохраняет изменения
 
-    def __add__(self, field_player1: str, field_player2: str, one_or_zero: int):
-        self.cursor.execute("INSERT into save_field values (?, ?, ?)", (field_player1, field_player2, one_or_zero))
+    def __add__(self, field_player1: str, field_player2: str, one_or_zero: int, number_game: str):
+        self.cursor.execute("INSERT into save_field_" + number_game + " values (?, ?, ?)",
+                            (field_player1, field_player2, one_or_zero))
         # добавляет значение в столбцы
+        self.con.commit()
 
-    def __del__(self):
-        self.cursor.execute("DELETE FROM save_field")  # удаляет всю инфу с таблицы
+    def __del__(self, number_game: str):
+        self.cursor.execute("DROP TABLE save_field_" + number_game)  # удаляет таблицу
+        self.con.commit()
 
-    def save(self):
-        self.con.commit()  # сохраняет таблицу обязательно делай иначе ничего не сохранится
-
-    def unpacking(self):
+    def unpacking(self, number_game):
+        # извлекает последние внесенные изменения
         for row in self.cursor.execute(
-                "SELECT rowid, * FROM save_field ORDER BY player1"):  # кастыльный но рабочий метод извлечения инфы
+                "SELECT rowid, * FROM save_field_" + number_game + " ORDER BY player1"):
             tuple_info = row
         player_field_1 = json.loads(tuple_info[1])  # начинаем не с нуля так как на нулевом месте номер строки
         player_field_2 = json.loads(tuple_info[2])
